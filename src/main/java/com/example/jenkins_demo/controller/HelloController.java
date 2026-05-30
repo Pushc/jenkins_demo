cc6814a0532ad6150c421183aafe2131e1496c22
Pushc <pushkar8983@gmail.com>
2026-05-30 12:10:22 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/example/jenkins_demo/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.jenkins_demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HelloController {
+
+ @GetMapping("/")
+ public String home() {
+   return "Hello from Jenkins + Docker";
+ }
+
+}
